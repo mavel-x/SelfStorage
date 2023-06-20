45c4cfe91bd0521c7c87f822df09ce307878d089
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-06-21 00:20:59 +0300

    Updated models for the "account" app.

--- account/models.py
+++ account/models.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from phonenumber_field.modelfields import PhoneNumberField
 
-
 class User(AbstractUser):
     phone = PhoneNumberField('Телефон')
-    avatar = models.ImageField('Аватар', blank=True)
+    avatar = models.ImageField(
+        verbose_name='Аватар',
+        blank=True,
+        null=True,
+        upload_to='images/avatars',
+        validators=[FileExtensionValidator(
+            ['jpg', 'jpeg', 'png',  'svg', 'webp']),
+        ],
+    )
 
 
 @receiver(pre_delete, sender=User)
