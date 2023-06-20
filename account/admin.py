eefc5bbd7de1e626c79930e2e8e22dbce2c10b2b
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-06-21 00:22:22 +0300

    Updated admin's panel for the "account" app.

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from account.models import User
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    ...
+    list_display = (
+        'username',
+        'phone',
+        'preview',
+    )
+    list_per_page = 20
+    readonly_fields = ('preview',)
+
+    @admin.display(description='Превью аватара')
+    def preview(self, obj):
+        if obj.avatar:
+            return mark_safe(
+                f'<img src="{obj.avatar.url}" style="max-height: 100px;">'
+            )
