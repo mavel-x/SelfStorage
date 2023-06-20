76c00f504197f82d4cac6de1a54dac9034b34504
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-06-21 00:22:29 +0300

    Updated admin's panel for the "storage" app.

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,78 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from storage.models import (
+    Storage,
+    Image,
+    Box,
+    Booking,
+    Discount,
+)
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    list_display = (
+        'address',
+        'preview',
+    )
+    list_per_page = 20
+    readonly_fields = ('preview',)
+
+    @admin.display(description='Превью склада')
+    def preview(self, obj):
+        if obj.images:
+            return mark_safe(
+                f'<img src="{obj.images.get(order=1).image.url}" style="max-height: 100px;">'
+            )
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = (
+        'storage',
+        'order',
+        'preview',
+    )
+    list_per_page = 20
+    readonly_fields = ('preview',)
+
+    @admin.display(description='Превью изображения')
+    def preview(self, obj):
+        return mark_safe(
+            f'<img src="{obj.image.url}" style="max-height: 100px;">'
+        )
+
+
+@admin.register(Box)
+class BoxAdmin(admin.ModelAdmin):
+    list_display = (
+        'storage',
+        'number',
+        'price',
+    )
+    list_per_page = 20
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'box',
+        'amount',
+        'payment_status',
+    )
+    readonly_fields = [
+        'amount',
+    ]
+    list_per_page = 20
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = (
+        'promocode',
+        'percent',
+        'money',
+    )
+    list_per_page = 20
