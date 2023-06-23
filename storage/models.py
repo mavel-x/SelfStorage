3e4ae65590de90c3ca11538f4b4cea2775e2c17e
nikolay <vasadaz@ya.ru>
2023-06-23 11:10:06 +0300

    Updated models for the “storage” app.

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.core.validators import (
     MaxValueValidator,
     MinValueValidator,
@@ -6,6 +8,7 @@ from django.core.validators import (
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
+from django.utils import timezone
 
 from account.models import User
 
@@ -243,8 +246,7 @@ class Lead(models.Model):
     )
     date = models.DateField(
         'Дата обращения',
-        blank=True,
-        null=True,
+        default=timezone.now,
     )
     description = models.CharField(
         'Описание',
