0680ee6c98a607edbc2c41414c187ec5f9e8166f
nikolay <vasadaz@ya.ru>
2023-06-23 17:15:36 +0300

    Created forms.py

---
+++ storage/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Lead
+
+
+class LeadForm(forms.ModelForm):
+
+    class Meta:
+        model = Lead
+        fields = ('email', 'name', 'description')
+
