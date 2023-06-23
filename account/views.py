1f7d80fc3deaa106b80847490e4ca3588d07f3cb
mavel <p.m.berdnikov@gmail.com>
2023-06-23 18:19:48 +0200

    add working account change form

--- account/views.py
+++ account/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
@@ -8,7 +6,7 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView
 
 from .models import User
-from .forms import LoginForm, SignUpForm
+from .forms import LoginForm, SignUpForm, AccountChangeForm
 
 
 def get_display_name(user: User):
@@ -32,6 +30,25 @@ class AccountView(TemplateView):
         })
 
 
+class AccountChangeView(FormView):
+    form_class = AccountChangeForm
+    template_name = 'my-rent.html'
+    success_url = reverse_lazy('account')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form: AccountChangeForm):
+        form.update_user()
+        messages.success(self.request, 'Данные сохранены.')
+        return JsonResponse({'status': 'ok'})
+
+    def form_invalid(self, form):
+        return JsonResponse({'status': 'error', 'form_errors': form.errors})
+
+
 class LoginView(FormView):
     form_class = LoginForm
     template_name = 'index.html'
@@ -48,7 +65,7 @@ class LoginView(FormView):
         return JsonResponse({'status': 'error', 'errors': 'Неверный логин или пароль.'})
 
     def form_invalid(self, form):
-        return JsonResponse({'status': 'error', 'errors': form.errors})
+        return JsonResponse({'status': 'error', 'form_errors': form.errors})
 
 
 class SignUpView(FormView):
@@ -67,7 +84,6 @@ class SignUpView(FormView):
         print(form.errors)
         return JsonResponse({
             'status': 'error',
-            # 'errors':  json.dumps(form.errors, ensure_ascii=False),
             'errors': form.errors,
         })
 
--- static/js/main.js
+++ static/js/main.js
@@ -55,7 +55,11 @@ $(document).on('submit', '#signup-form', function (e){
 document.getElementById('account-edit-button').addEventListener('click', e => {
     e.preventDefault()
     // document.getElementById('account-email').disabled = false
-    document.getElementById('account-phone').disabled = false
+    let phoneField = document.getElementById('account-phone')
+    phoneField.disabled = false
+    if (phoneField.value === 'не указан') {
+        phoneField.value = '';
+    }
     document.getElementById('account-password-old').disabled = false
     document.getElementById('account-edit-button').style.display = 'none'
     document.getElementById('account-save-button').style.display = 'inline-block'
@@ -65,11 +69,56 @@ document.getElementById('account-edit-button').addEventListener('click', e => {
     document.getElementById('account-password-old').value = ''
 })
 
-document.getElementById('account-save-button').addEventListener('click', () => {
-    setTimeout(() => {
-        document.getElementById('account-phone').disabled = true
-        document.getElementById('account-password1').disabled = true
-        document.getElementById('account-edit-button').style.display = 'inline-block'
-        document.getElementById('account-save-button').style.display = 'none'
-    }, 0)
+
+function displayErrors(errors, errorElement) {
+    if (errors) {
+        var errorList = $('<ul class="errorlist long-form-errors"></ul>');
+        for (var i = 0; i < errors.length; i++) {
+            var errorItem = $('<li></li>').text(errors[i]);
+            errorList.append(errorItem);
+        }
+        errorElement.html(errorList);
+    }
+}
+
+$(document).on('submit', '#account-change-form', function (e) {
+    e.preventDefault();
+    $('#account-change-form-errors').html('');
+    let errorElements = document.querySelectorAll('[id^="error_"]');
+    errorElements.forEach(function(element) {
+        element.innerHTML = '';
+    });
+    let csrftoken = Cookies.get('csrftoken');
+    $.ajax({
+        type: 'POST',
+        url: '/account/change/',
+        headers: {'X-CSRFToken': csrftoken},
+        data: {
+            first_name: $('#account-first-name').val(),
+            last_name: $('#account-last-name').val(),
+            email: $('#account-email').val(),
+            phone: $('#account-phone').val(),
+            password1: $('#account-new-password1').val(),
+            password2: $('#account-new-password2').val(),
+            password_old: $('#account-password-old').val(),
+        },
+        success: function (data) {
+            if (data.status === 'error') {
+                $('#account-change-form-errors').html(data.errors);
+                if (data.form_errors) {
+                    // Display errors for each field
+                    for (var field in data.form_errors) {
+                        if (data.form_errors.hasOwnProperty(field)) {
+                            var errorElement = $('#error_account_' + field);
+                            displayErrors(data.form_errors[field], errorElement);
+                        }
+                    }
+                }
+            } else if (data.status === 'ok') {
+                $('#account-phone').prop('disabled', true);
+                $('#account-password-old').prop('disabled', true);
+                location.reload();
+            }
+        }
+    })
 })
