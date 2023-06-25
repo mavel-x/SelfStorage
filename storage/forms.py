6268383085543e275d3b67b51405b157f8733d23
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2023-06-25 22:56:22 +0300

    Created form PaymentForm

--- storage/forms.py
+++ storage/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Lead
+from .models import Booking, Lead
 
 
 class LeadForm(forms.ModelForm):
@@ -7,4 +7,13 @@ class LeadForm(forms.ModelForm):
     class Meta:
         model = Lead
         fields = ('email', 'name', 'description')
+
+
+class PaymentForm(forms.ModelForm):
+    email = forms.EmailField()
+    promocode = forms.CharField(max_length=10, required=False)
+
+    class Meta:
+        model = Booking
+        fields = ('start_date', 'end_date')
 
