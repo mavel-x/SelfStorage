c9d55b0493bfc50a8e04983e189e0e7d766f6a45
mavel <p.m.berdnikov@gmail.com>
2023-06-26 18:08:42 +0200

    more prod settings

--- SelfStorage/settings.py
+++ SelfStorage/settings.py
@@ -95,9 +95,9 @@ if DEBUG:
 else:
     DEFAULT_DATABASE = {
         'ENGINE': env.str('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': env.str('DB_NAME'),
-        'USER': env.str('DB_USER'),
-        'PASSWORD': env.str('DB_PASSWORD'),
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
         'HOST': env.str('DB_HOST', '127.0.0.1'),
         'PORT': env.str('DB_PORT', '5432'),
     }
