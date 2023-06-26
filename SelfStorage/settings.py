63bd46e4bde9c9b5b639b6de84825304163dc77b
NSysoev(eduserv) <vasadaz@ya.ru>
2023-06-26 16:49:46 +0000

    Server maker

--- SelfStorage/settings.py
+++ SelfStorage/settings.py
@@ -144,8 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (BASE_DIR / 'static',)
-STATIC_ROOT = 'collected_static'
+STATICFILES_DIRS = [
+      # BASE_DIR / 'static',
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
