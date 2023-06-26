6fb169e304558fd15f2831ca80375ea6607eed5d
mavel <p.m.berdnikov@gmail.com>
2023-06-26 17:47:00 +0200

    add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ "$EUID" -ne 0 ]
+  then
+    echo "Please run as root."
+    exit
+fi
+
+git pull
+venv/bin/pip install -U -r requirements.txt
+venv/bin/python manage.py collectstatic --noinput
+venv/bin/python manage.py makemigrations --noinput
+venv/bin/python manage.py migrate --noinput
+systemctl restart selfstorage-django.service
+systemctl reload nginx
+
+echo "Project updated."
