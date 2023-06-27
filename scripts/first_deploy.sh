e54be0aa9a878337190609848d9eeabbb1cbc198
mavel <p.m.berdnikov@gmail.com>
2023-06-28 00:08:56 +0200

    make script stop asking for input

--- scripts/first_deploy.sh
+++ scripts/first_deploy.sh
@@ -9,9 +9,9 @@ fi
 
 # Install Docker
 apt-get update
-apt-get install ca-certificates curl gnupg
+apt-get install -y ca-certificates curl gnupg
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
