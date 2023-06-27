d42263c0d06cd01971a7a5a902552d4674806cfc
mavel <p.m.berdnikov@gmail.com>
2023-06-28 00:44:53 +0200

    update README and remove redundant actions from local_installer.py

--- local_installer.py
+++ local_installer.py
@@ -1,18 +1,16 @@
 import os
 
-def main():
-    os.system('python -m pip install -r requirements.txt')
 
+def main():
     if not os.path.isfile('.env'):
         from django.core.management.utils import get_random_secret_key
         secret_key = get_random_secret_key()
         with open('.env', 'w') as file:
             file.write(f'DJANGO_SECRET_KEY={secret_key}')
 
-    os.system('python manage.py migrate')
-
     if not os.path.isdir('media'):
         os.mkdir('media')
+
 
 if __name__ == '__main__':
     main()
