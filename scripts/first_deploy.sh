5ff3c8df170a8a0a8b88642743652ae3c607695b
mavel <p.m.berdnikov@gmail.com>
2023-06-27 23:58:51 +0200

    stop scripts asking for input, update docker installation

--- scripts/first_deploy.sh
+++ scripts/first_deploy.sh
@@ -8,12 +8,18 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Install Docker
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get remove $pkg; done
 apt-get update
 apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+install -y -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 apt-get install -y nginx
 
--- scripts/init_postgres.sh
+++ scripts/init_postgres.sh
@@ -14,7 +14,7 @@ if [ $( docker ps -a | grep selfstorage-postgres | wc -l ) -gt 0 ]
     exit
 fi
 
-apt-get install libpq-dev -y
+apt-get install -y libpq-dev
 
 docker run -d --name selfstorage-postgres \
 --env-file .env -p 5432:5432 \
