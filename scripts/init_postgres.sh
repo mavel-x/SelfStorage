fee0e6138079acd625b817f0ac5e35e6fd251222
mavel <p.m.berdnikov@gmail.com>
2023-06-27 18:19:45 +0200

    add a loop to wait for container

--- scripts/init_postgres.sh
+++ scripts/init_postgres.sh
@@ -18,7 +18,13 @@ docker run -d --name selfstorage-postgres \
 --env-file .env -p 5432:5432 \
 -v selfstorage_postgres:/var/lib/postgresql/data \
 postgres:14
-sleep 1
+
+# Wait for the PostgreSQL container to be ready
+until docker exec selfstorage-postgres pg_isready; do
+  sleep 1
+done
+
+venv/bin/python ./manage.py makemigrations --noinput
 venv/bin/python ./manage.py migrate --noinput
 venv/bin/python manage.py upload_test_data
 docker stop -t 10 selfstorage-postgres
